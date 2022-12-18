03463c741b988d84159f10a62b6815e9c18b3d69
Brendson <brendson.net@gmail.com>
2022-12-18 16:47:09 -0300

    feat: criando autenticação via github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
-import type { AppProps } from 'next/app'
-import { Header } from '../components/Header'
+import type {AppProps} from "next/app";
+import {SessionProvider as NextAuthprovicer} from "next-auth/react";
+import {Header} from "../components/Header";
 
 import '../styles/global.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <>
+        <NextAuthprovicer session={pageProps.session}>
             <Header />
             <Component {...pageProps} />
-        </>
+        </NextAuthprovicer>
     )
 }
