b4742b1b15ffabc761f26a7105d24f4e6282e017
Brendson <brendson.net@gmail.com>
2023-01-10 15:13:56 -0300

    feat: criando query de inserção de usuario no banco fauna

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,9 @@
 // @ts-nocheck
-
+import {query as q} from "faunadb";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+import {fauna} from "../../../services/fauna";
 
 export const authOptions = {
     providers: [
@@ -11,5 +13,24 @@ export const authOptions = {
             scope: "read:user"
         }),
     ],
+    callbacks: {
+        async signIn({ user }) {
+            const { email } = user;
+
+            try {
+                await fauna.query(
+                    q.Create(
+                        q.Collection('users'),
+                        { data: { email } }
+                    )
+                )
+
+                return true
+            } catch {
+                return false
+            }
+
+        },
+    }
 }
 export default NextAuth(authOptions)
